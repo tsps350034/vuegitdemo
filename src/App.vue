0743b5556671ad71716d03b4582423cba8912030
Cho Chen-Chi <tsps350034@gmail.com>
2020-12-17 17:26:26 +0800

    TEST

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
     <router-link to="/about">About</router-link>
     <router-link to="/about">Hello</router-link>
     <router-link to="/about">Hello</router-link>
+    
   </div>
   <router-view/>
 </template>
